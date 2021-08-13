de6b8d0105a22141be56a845f1e68281ee79aca8
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-08-13 00:41:28 -0400

    Incorporate Mocha and Chai on lotide

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,4 @@
+const { assert } = require('chai');
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
@@ -10,5 +11,26 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
 
 assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]); // => [2, 3]
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]); // => [3, 4]
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
 
  
